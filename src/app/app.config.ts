d26d3bb9fcf098de4636891d323e3a63edcd960f
Mariam Safwat <mariemsafwat20@gmail.com>
2025-10-27 13:27:32 +0300

    PrimeNG installed and working.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,21 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// PrimeNG imports
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
